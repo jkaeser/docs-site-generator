1acf2928a77583f49d7bb0c3f2dcca49f5ce53ca
John Kaeser <jakaeser44@gmail.com>
2019-02-21 12:40:48 -0500

    CYS_PS1-4 Add script to download content bundles.

---
+++ scripts/get-content.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+# Install instructions:
+# On mac, install pip with sudo easy_install pip
+# On debian, apt-get install pip
+# Then run sudo pip install requests
+
+import subprocess
+import sys
+import os
+import shutil
+import contextlib
+import colorama
+
+from contextlib import contextmanager
+from colorama import Fore, Back, Style, init
+
+# Install with: pip install requests
+try:
+  import requests
+except:
+  subprocess.call('pip install requests', shell=True)
+  import requests
+
+repos = [
+    {
+        "name": "product-A",
+        "git": "https://github.com/jkaeser/docs-content.git"
+    },
+    {
+        "name": "product-B",
+        "git": "https://github.com/jkaeser/docs-content.git"
+    }
+]
+
+# Make colors reset after every line by default.
+init(autoreset=True)
+
+# See https://stackoverflow.com/questions/431684/how-do-i-change-directory-cd-in-python#answer-24176022
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+def request_include(r):
+    return raw_input(Fore.GREEN + "\nInclude content from " + r["name"] + "? [y/n]: ")
+
+def request_branch():
+    return raw_input(Fore.GREEN + "Which branch or tag would you like to check out (defaults to master)? ")
+
+def request_remove(r):
+    return raw_input(Fore.YELLOW + "It looks like " + r["name"] + " has already been added. Would you like to remove it? [y/n]: ")
+
+def add_repo(r):
+    # Prompt user to include content from this repo or not
+    use = request_include(r)
+
+    # Handle incorrect inputs
+    while use.lower() not in ["y", "n"]:
+        print(use + " is not a valid response. Please enter \"y\" or \"n\"")
+        use = request_include(r)
+
+    # Conditionally download content
+    if use.lower() == "y":
+        # Create directory
+        if not os.path.exists(r["name"]):
+            print("Making new directory named " + r["name"])
+            subprocess.call("mkdir " + r["name"] , shell=True)
+        os.chdir(r["name"])
+
+        # Initialize repo and grab branches
+        if os.path.exists(".git"):
+            print("Git repo already initialized.\n")
+            subprocess.call("git fetch --all", shell=True)
+
+            print(Fore.CYAN + "Branches:")
+            subprocess.call("git branch --list -a", shell=True)
+            print(Fore.CYAN + "\nTags:")
+            subprocess.call("git tag --list", shell=True)
+
+        else:
+            subprocess.call("git init", shell=True)
+            subprocess.call("git remote add origin " + r["git"], shell=True)
+            subprocess.call("git fetch --all", shell=True)
+
+        # Request branch
+        branch = request_branch()
+        # Default to master branch
+        if branch == "":
+            branch = "master"
+            print "Defaulting to master branch."
+
+        # Check out selected branch or tag
+        subprocess.call("git checkout " + branch, shell=True)
+        print "\n"
+
+        # Go back a level for any other repos that will be added.
+        os.chdir("..")
+
+    if use.lower() == "n":
+        # Present opportunity to remove content.
+        if os.path.exists(r["name"]):
+            remove = request_remove(r)
+
+            while remove.lower() not in ["y", "n"]:
+                print(use + " is not a valid response. Please enter \"y\" or \"n\"")
+                remove = request_remove(r)
+
+            if remove.lower() == "y":
+                shutil.rmtree(r["name"])
+                print(r["name"] + " content removed.")
+            else:
+                print("Keeping content.")
+
+def get_content():
+    # This script must be run from within the /scripts directory.
+    # @TODO: Figure out how to make this path-independent.
+    if os.path.basename(os.getcwd()) != "scripts":
+        sys.exit(Fore.RED + "ERROR: You must run " + os.path.basename(__file__) + " from the scripts directory.")
+
+    # This relative path is the reason we require the script to be run from a particular place.
+    with cd("../src/pages/docs"):
+        for r in repos:
+            add_repo(r)
+
+get_content()
